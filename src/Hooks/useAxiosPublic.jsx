0b1b63a919f96427b8d3bce67a0cba25b81438af
Alien Brain <murad.dub@gmail.com>
2023-12-22 16:11:22 +0600

    delete a task done

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
   baseURL:
-    // "http://localhost:5000",
-    "https://sync360-task-management-server.vercel.app/",
+    "http://localhost:5000",
+    // "https://sync360-task-management-server.vercel.app/",
 });
 
 const useAxiosPublic = () => {
--- src/Pages/Dashboard/AllTasks/TaskDisplayCard.jsx
+++ src/Pages/Dashboard/AllTasks/TaskDisplayCard.jsx
@@ -1,5 +1,33 @@
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 const TaskDisplayCard = ({ task }) => {
   const { _id, title, description, deadline, priority } = task || {};
+  const axiosPublic = useAxiosPublic();
+  const handleDeleteTask = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((res) => {
+      if (res.isConfirmed) {
+        axiosPublic.delete(`/allTasks/${id}`).then((result) => {
+          if (result.data.deletedCount > 0) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "This Task has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div className="card text-sm border border-zinc-900/25 shadow-xl p-3">
       <div className="flex justify-center items-center gap-0">
@@ -22,6 +50,12 @@ const TaskDisplayCard = ({ task }) => {
         <p className="font-bold bg-gradient-to-r from-pink-500 via-red-500 to-red-900 bg-clip-text text-transparent">
           Task Deadline: {deadline}
         </p>
+        <button
+          onClick={() => handleDeleteTask(_id)}
+          className="btn btn-ghost btn-lg"
+        >
+          <FaTrashAlt className="text-red-600"></FaTrashAlt>
+        </button>
       </div>
     </div>
   );
